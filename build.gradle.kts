a0f161aacd45ee1cd27ffa3a61afcd5b64399b2b
Connor Wenck <none@example.com>
2021-08-01 15:24:52 -0400

    Core initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.5.20"
+    application
+}
+
+group = "dev.cwenck.music-manager"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation("com.google.oauth-client:google-oauth-client-jetty:1.30.6")
+    implementation("com.google.apis:google-api-services-sheets:v4-rev581-1.25.0")
+}
+
+tasks.test {
+    useTestNG()
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "11"
+}
+
+application {
+    mainClass.set("core.MainKt")
+}
